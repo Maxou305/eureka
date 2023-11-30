e7e2881811f68740f3d45d72c5b7ec087f2fa45f
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2023-11-30 09:53:55 +0100

    first commit

---
+++ src/test/java/com/webapp/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
